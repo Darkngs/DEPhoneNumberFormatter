9200af8f2114ef5e7e59042e847e6667b7b5999b
Mykola Golyash <mykola@golyash.com>
2020-01-21 19:23:25 +0200

    Update to modern Swift.

--- DEPhoneNumberFormatter/AppDelegate.swift
+++ DEPhoneNumberFormatter/AppDelegate.swift
@@ -3,29 +3,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
    
-   var window: UIWindow?
-   
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       return true
    }
    
-   func applicationWillResignActive(_ application: UIApplication) {
-      
+   // MARK: -
+   
+   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+      return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
    }
    
-   func applicationDidEnterBackground(_ application: UIApplication) {
-      
-   }
-   
-   func applicationWillEnterForeground(_ application: UIApplication) {
-      
-   }
-   
-   func applicationDidBecomeActive(_ application: UIApplication) {
-      
-   }
-   
-   func applicationWillTerminate(_ application: UIApplication) {
+   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
       
    }
 }
